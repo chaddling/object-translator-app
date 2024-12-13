175043d2fec169ee9d55bcdc093f36be7bddeef3
Chad Gu <chad.m.gu@gmail.com>
2024-12-12 20:48:49 -0500

    formatting

--- app/main.py
+++ app/main.py
@@ -6,11 +6,13 @@ from stream.coroutines import do_streaming
 
 
 stream = WrappedVideoStream.open(0)
-container = st.empty() # What's the type hint?
+container = st.empty()  # What's the type hint?
+
 
 async def main():
     while True:
         await do_streaming(stream, container)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
